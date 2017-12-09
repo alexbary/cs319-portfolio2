dd0f287ac1ec1b68c384093aef0cdd164802fe6b
sydehlin <sydehlin@iastate.edu>
2017-12-09 15:13:49 -0600

    implementing redis

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/redis', function(){
+  print_r(app()->make('redis'));
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
